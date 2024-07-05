436fd545784d9168ff38cad2d1bf9bd931422ece
MisterCbing <da@susanin.news>
2024-07-05 17:27:04 +0400

    v 0.1_1

--- temp.py
+++ temp.py
@@ -3,25 +3,47 @@ from threading import Thread
 from time import sleep
 
 class Cafe:
+
     def __init__(self, table_list):
+        self.customers = []
         self.q = queue.Queue()
         self.table_list = table_list
-        self.с = []
 
     def customer_arrival(self):
         for i in range(1, 21):
             print(f'Посетитель номер {i} прибыл')
             self.serve_customer(i)
             sleep(1)
+        print(self.q)
 
     def serve_customer(self, i):
-        for t in tables:
-            if not t.is_busy:
-                Customer(i, t).start()
-                break
-        self.q.put(i)
+        # self.q.put(i)
+        # if any(not t.is_busy for t in self.table_list):
+        #     j = self.q.get()
+        #     for t in tables:
+        #         if not t.is_busy:
+        #             self.customers.append(Customer(j, t))
+        #             self.customers[-1].start()
+        #             break
+        # if not self.q.empty():
+        #     print(f'Посетитель номер {i} ожидает свободный стол.')
 
-    def waiting(self, i):
+
+    def run(self):
+        while True:
+
+            try:
+                i = self.q.get(timeout=1)
+                for t in tables:
+                    if not t.is_busy:
+                        self.customers.append(Customer(i, t))
+                        self.customers[-1].start()
+                        break
+            except:
+                break
+            for c in self.customers:
+                c.join()
+
 
 
 class Table:
@@ -38,15 +60,17 @@ class Customer(Thread):
         self.table = table
 
     def run(self):
-        print(f'Пришёл посетитель {self.number} за столик {self.table.number}.')
-        self.table.is_busy = True
-        sleep(5)
-        print(f'Ушёл посетитель {self.number} из-за столика {self.table.number}.')
-        self.table.is_busy = False
+        # print(f'Посетитель номер {self.number} сел за стол {self.table.number}.')
+        # self.table.is_busy = True
+        # sleep(5)
+        # print(f'Посетитель номер {self.number} покушал и ушёл. ')
+        # self.table.is_busy = False
 
-tables = []
-for i in range(1, 21):
-    tables.append(Table(i))
+
+table1 = Table(1)
+table2 = Table(2)
+table3 = Table(3)
+tables = [table1, table2, table3]
 
 # Инициализируем кафе
 cafe = Cafe(tables)
@@ -57,13 +81,3 @@ customer_arrival_thread.start()
 
 # Ожидаем завершения работы прибытия посетителей
 customer_arrival_thread.join()
-
-# cc = []
-# for i in range(10):
-#     cc.append(Customer(i, tables[i]))
-#
-# for i in cc:
-#     i.start()
-#
-# for i in cc:
-#     i.join()
