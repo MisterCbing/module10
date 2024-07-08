d3dbe31e20d80a05bd0aeef06e322a80b3fc812d
MisterCbing <da@susanin.news>
2024-07-08 13:54:28 +0400

    v 1.0

--- module10_warehouse_queue.py
+++ module10_warehouse_queue.py
@@ -4,9 +4,8 @@ from multiprocessing import Process, Pipe, Queue
 class WarehouseManager:
     data = {}
 
-
-    def process_request(self, *request):
-        request, temp = request[0], request[1].get()
+    def process_request(self, request, q):
+        temp = q.get()
         if request[1] == "receipt":
             temp[request[0]] = temp.get(request[0], 0) + request[2]
         elif request[1] == "shipment":
@@ -14,12 +13,13 @@ class WarehouseManager:
         q.put(temp)
 
     def run(self, requests):
-
+        q.put(self.data)
         for i in requests:
             proc = Process(target=self.process_request, args=(i, q))
             proc.start()
             proc.join()
         self.data = q.get()
+
 
 if __name__ == '__main__':
     q = Queue()
